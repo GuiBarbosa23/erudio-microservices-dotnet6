41d3f035d0a9f318f180f28f3cf6b8750b9009f3
GuiBarbosa23 <guibarbosadeveloper@gmail.com>
2022-04-28 22:42:48 -0300

    Sessão 10 finalizada do curso

---
+++ src/S010_MicroServices_Net6/GeekShopping/GeekShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekShopping.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegistersMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+
+            });
+            return mappingConfig;
+        }
+    }
+}
